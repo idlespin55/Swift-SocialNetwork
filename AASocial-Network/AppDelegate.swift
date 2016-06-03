0d921ed6d84b3e21b512b72320502e3b543cbd72
idlespin55 <ajmal.ahmady@gmail.com>
2016-06-03 11:02:20 +0430

    Completed Part 4:  FB integration

--- AASocial-Network/AppDelegate.swift
+++ AASocial-Network/AppDelegate.swift
@@ -4,9 +4,30 @@
 //
 //  Created by Ajmal Ahmady on 6/1/16.
 //  Copyright © 2016 FrontierGroup. All rights reserved.
-//
+//  FB INTEGRATION: (1) Create a new project in xCode and Firebase
+//                  (2) At developers.facebook.com, create a new app, enter xCode project display name and bundle ID,
+//                      & create a new product (FB login).  Click yes for 'Client OAuth Login' option
+//                  (3) Enter the FB App ID & App Secret in the Firebase/Login&Auth/FB screen
+//                  (4) Download the FB SDK, and then copy the Bolts, CoreKit, and LoginKit into your xCode project
+//                  (5) To use Firebase: 'Pod Init', enter "pod 'Firebase', '2.3.3'", then 'Pod Install'
+//                  (6) Open Info.plist as source code
+//                  (7) Copy the following keys in Info.plist: LSApplicationQueriesSchemes, NSAppTransportSecurity, & CFBundleURLTypes
+//                  (8) In the project build settings, change Bitcode setting to 'No'
+//                  (9) In AppDelegate.swift, import FBSDKCoreKit and FBSDKLoginKit
+//                  (10) In didFinishLaunchingWithOptions, send data to FB to see if user is already logged in by entering:
+//                      'return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)'
+//                  (11) In didBecomeActive, reactivate FB if needed by entering 'FBSDKAppEvents.activateApp()'
+//                  (12) Create a new function to pass the application, url, etc to FB: func application
+//                      (application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {}
+//                  (13) In the VC, import FBSDKCoreKit/FBSDKLonginKit and create IBAction with appropriate code
+//                  Useful Video: Part 4
+//                  Useful Websites: https://www.firebase.com/docs/ios/guide/login/facebook.html
+//                                   https://developers.facebook.com/docs/ios/getting-started
+
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +36,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+        // Override point for customization after application launch. Check if user is already logged in
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -34,13 +55,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        //Reactivate FB if not already activated
+        FBSDKAppEvents.activateApp()
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        //This will open the FB url
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+    }
 }
-
---
+++ Pods/Target Support Files/Pods-AASocial-Network/Pods-AASocial-Network-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AASocial_NetworkVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AASocial_NetworkVersionString[];
+
